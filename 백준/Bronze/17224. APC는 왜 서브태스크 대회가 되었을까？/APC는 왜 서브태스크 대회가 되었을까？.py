58b758b997d670a70476303f26f6495078a92abe
Jaewook Shin <59306720+jwshin0908@users.noreply.github.com>
2023-01-24 16:50:00 +0900

    [Bronze I] Title: APC는 왜 서브태스크 대회가 되었을까?, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/17224. APC는 왜 서브태스크 대회가 되었을까？/APC는 왜 서브태스크 대회가 되었을까？.py
+++ 백준/Bronze/17224. APC는 왜 서브태스크 대회가 되었을까？/APC는 왜 서브태스크 대회가 되었을까？.py
@@ -1,7 +1,9 @@
+# 난이도를 sub2 다음 sub1 순으로 정렬
+
 N, L, K = map(int, input().split())
 result = 0
 question = []
-num = K
+num = 0
 
 for i in range(N):
     question.append(list(map(int, input().split())))
@@ -11,11 +13,11 @@ question.sort(key=lambda x: (x[1],x[0]))
 for i in question:
     if i[1]<=L:
         result+=140
-        num-=1
+        num+=1
     elif i[0]<=L:
         result+=100
-        num-=1
-    if num==0:
+        num+=1
+    if num==K:
         break
 
 print(result)
