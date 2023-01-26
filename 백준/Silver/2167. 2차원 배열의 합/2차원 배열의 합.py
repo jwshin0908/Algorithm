ae75da535d7b3b8623446f33055db7a38b1aa55c
Jaewook Shin <59306720+jwshin0908@users.noreply.github.com>
2023-01-27 03:05:53 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 992 ms, Memory: 36992 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -14,3 +14,24 @@ k = int(input())
 for _ in range(k):
     i, j, x, y = map(int, input().split())
     print(sum_array[x][y] - sum_array[x][j - 1] - sum_array[i - 1][y] + sum_array[i - 1][j - 1])
+    
+    
+    
+# 시간 초과 코드
+N, M = map(int, input().split())
+array = []
+for _ in range(N):
+  array.append(list(map(int, input().split())))
+K = int(input())
+sum_array = []
+
+for _ in range(K):
+  sum_array.append(list(map(int, input().split())))
+
+# [i,j,x,y]을 통해 행 index가 range(i-1, x), 열 index가 range(j-1, y) 이내 array 누적 합
+for i in range(K):
+  sum = 0
+  for j in range(sum_array[i][0] - 1, sum_array[i][2]):
+    for k in range(sum_array[i][1] - 1, sum_array[i][3]):
+      sum += array[j][k]
+  print(sum)
