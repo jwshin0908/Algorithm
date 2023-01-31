f1509cb363ecc56d5f58ca713d8fb5b2641e9168
Jaewook Shin <59306720+jwshin0908@users.noreply.github.com>
2023-01-31 18:22:32 +0900

    Add files via upload

---
+++ python-for-coding-test/ch05_DFS_BFS/5-5.py
@@ -0,0 +1,17 @@
+# 5-5. 2가지 방식으로 구현한 팩토리얼 예제(p.132)
+
+# 반복을 통해 구현한 n!
+def factorial_iterative(n):
+  result = 1
+  for i in range(1, n + 1):
+    result *= i
+  return result
+
+# 재귀함수를 통해 구현한 n!
+def factorial_recursive(n):
+  if n <= 1:
+    return 1
+  return n * factorial_recursive(n - 1)
+
+print(factorial_iterative(5))
+print(factorial_recursive(5))
