a447bd17518048dab524d29e628b9de24e0c0a0c
Jaewook Shin <jwshin0908@naver.com>
2023-09-20 20:55:00 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+d = [0] * 46
+
+d[0] = 0
+d[1] = 1
+
+for i in range(2, n + 1):
+    d[i] = d[i - 1] + d[i - 2]
+    
+print(d[n])
