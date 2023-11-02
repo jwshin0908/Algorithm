7e241daedd6dbaec826e0d84aa4c6277890f1a91
Jaewook Shin <jwshin0908@naver.com>
2023-11-02 21:25:01 +0900

    [Silver II] Title: N과 M (11), Time: 492 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().rstrip().split())
+array = sorted(list(set(map(int, input().rstrip().split()))))
+s = []
+
+def backtracking():
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    else:
+        for i in array:
+            s.append(i)
+            backtracking()
+            s.pop()
+            
+backtracking()
