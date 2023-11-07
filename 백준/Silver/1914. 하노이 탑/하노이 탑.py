0f5205021c449330fb6309cb48c06702512ec66b
Jaewook Shin <jwshin0908@naver.com>
2023-11-07 22:06:32 +0900

    [Silver I] Title: 하노이 탑, Time: 876 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+
+def func(n, start, end):
+    if n == 1:
+        print(start, end)
+        return
+    else:
+        func(n - 1, start, 6 - start - end)
+        print(start, end)
+        func(n - 1, 6 - start - end, end)
+        
+print(2 ** N - 1)
+
+if N <= 20:
+    func(N, 1, 3)
