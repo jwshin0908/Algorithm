4b69616572f22883062040eb992add24a30cf1a4
Jaewook Shin <jwshin0908@naver.com>
2023-11-18 16:21:33 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 124 ms, Memory: 109240 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -3,11 +3,9 @@ input = sys.stdin.readline
 
 n = int(input().rstrip())
 
-d = [0] * 1000001
-d[1] = 0
-d[2] = 1
+a, b = 0, 1
 
-for i in range(3, n + 1):
-    d[i] = (d[i - 2] + d[i - 1]) % 1000000007
+for i in range(n):
+    a, b = (a+b)%1000000007, a%1000000007
     
-print(d[n])
+print(a)
