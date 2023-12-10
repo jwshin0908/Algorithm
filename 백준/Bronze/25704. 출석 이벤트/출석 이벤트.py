688a43aefdc900eafa444f572decae6ae7e6a974
Jaewook Shin <jwshin0908@naver.com>
2023-12-11 06:37:56 +0900

    [Bronze IV] Title: 출석 이벤트, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25704. 출석 이벤트/출석 이벤트.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+price = int(input().rstrip())
+sale = [0]
+
+if N >= 5 :
+    sale.append(500)
+if N >= 10 :
+    sale.append(price // 10)
+if N >= 15 :
+    sale.append(2000)
+if N >= 20 :
+    sale.append(price // 4)
+
+result = price - max(sale)
+if result < 0:
+    result = 0
+print(result)
