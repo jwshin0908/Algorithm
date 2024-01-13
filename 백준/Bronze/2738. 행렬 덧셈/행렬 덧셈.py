f09b04f51c167b3d4ec7b85e5102394c1bc5683c
Jaewook Shin <jwshin0908@naver.com>
2024-01-13 09:31:13 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().rstrip().split())
+A, B = [], []
+
+for _ in range(N):
+    row = list(map(int, input().rstrip().split()))
+    A.append(row)
+    
+for _ in range(N):
+    row = list(map(int, input().rstrip().split()))
+    B.append(row)
+    
+for i in range(N):
+    for j in range(M):
+        print(A[i][j] + B[i][j], end = ' ')
+    print()    
